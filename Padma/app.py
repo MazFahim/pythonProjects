141a03c35b1bb3b9a3554409331adcb0bbf55e7f
Maz Fahim <mazfahim0@gmail.com>
2025-02-19 09:26:36 +0600

    Update app.py

--- Padma/app.py
+++ Padma/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
+from db import *
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/balance')
+def balance():
+    dailyData = get_daily_balances()
+    return render_template('balance.html', dailyData=dailyData)
+
+@app.route('/update_daily', methods=['POST'])
+def update_daily():
+    data = request.json
+    field, value = data.get("field"), data.get("value")
+    update_daily_balance(field, value)
+    updated_data = get_daily_balances()
+    return jsonify({"success": True, "balance": updated_data["balance"]})
 
 if __name__ == '__main__':
     app.run(debug=True)
