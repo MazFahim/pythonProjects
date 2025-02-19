71b1c752adff426781d8fcd459a87f5d4f7cd313
Maz Fahim <mazfahim0@gmail.com>
2025-02-19 09:26:17 +0600

    Update db.py

--- Padma/db.py
+++ Padma/db.py
@@ -1,16 +1,28 @@
 from pymongo import MongoClient
 from config import MONGO_URI
 
-
 client = MongoClient(MONGO_URI)
 db = client["padma"]
 collection = db["balances"]
-mydict = { "name": "John", "address": "Highway 37" }
 
-try:
-    print("MongoDB Connection Successful:", db.name)
-    x = collection.insert_one(mydict)
-    print(x)
 
-except Exception as e:
-    print("MongoDB Connection Failed:", e)
+def get_daily_balances():
+    data = collection.find_one({}, {"_id": 0})
+    if not data:
+        data = {"budget": 200, "expenses": 0}
+        data["balance"] = data["budget"] - data["expenses"]
+        collection.insert_one(data)
+    else:
+        data["balance"] = data["budget"] - data["expenses"]
+        collection.update_one({}, {"$set": {"balance": data["balance"]}})
+
+    return data
+
+def update_daily_balance(field, value):
+    value = int(value)
+    collection.update_one({}, {"$set": {field: value}}, upsert=True)
+
+    if field in ["budget", "expenses"]:
+        data = collection.find_one({}, {"_id": 0})
+        data["balance"] = data["budget"] - data["expenses"]
+        collection.update_one({}, {"$set": {"balance": data["balance"]}})
