d2fc4f9ff70cc4e8300042029ab3446dc6335cae
Maz Fahim <mazfahim0@gmail.com>
2025-04-21 08:53:45 +0600

    Update db.py

--- Padma/db.py
+++ Padma/db.py
@@ -5,8 +5,10 @@ from datetime import datetime
 client = MongoClient(MONGO_URI)
 db = client["padma"]
 collection = db["balances"]
+portfolio_collection = db["portfolio"]
 
 
+#balance functions
 def get_daily_balances():
     data = collection.find_one({}, {"_id": 0})
     today = datetime.today().strftime("%Y-%m-%d")
@@ -144,3 +146,35 @@ def update_balance_donation(field_type, field_name, value):
         {"$set": {f"{field_type}.{field_name}": value}},
         upsert=True
     )
+
+
+# Portfolio functions
+def get_portfolio():
+    data = portfolio_collection.find_one({}, {"_id": 0})
+    if not data:
+        data = {"type": "portfolio", "freelancing": [], "trading": []}
+        portfolio_collection.insert_one(data)
+    else:
+        updated_fields = {}
+        if "freelancing" not in data:
+            updated_fields["freelancing"] = []
+        if "trading" not in data:
+            updated_fields["trading"] = []
+        if updated_fields:
+            portfolio_collection.update_one({"_id": data["_id"]}, {"$set": updated_fields})
+            data.update(updated_fields)
+    return data
+
+
+def add_portfolio_item(category, item):
+    portfolio_collection.update_one({}, {"$push": {category: item}}, upsert=True)
+
+
+def update_portfolio_item(category, old_item, new_item):
+    portfolio_collection.update_one({}, {
+        "$set": {f"{category}.$[elem]": new_item}
+    }, array_filters=[{"elem": old_item}])
+
+
+def delete_portfolio_item(category, item):
+    portfolio_collection.update_one({}, {"$pull": {category: item}})
