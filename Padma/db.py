ad6f546020aedf7d4974885bea6bcfa0aed944ff
Maz Fahim <mazfahim0@gmail.com>
2025-07-02 14:16:12 +0600

    Added reorder functions and modifications in db.py

--- Padma/db.py
+++ Padma/db.py
@@ -203,12 +203,14 @@ def delete_portfolio_item(category, item):
 
 # Todo functions
 def get_all_todos():
-    todos = todos_collection.find({}, {"_id": 1, "text": 1})
-    return [{"_id": str(todo["_id"]), "text": todo["text"]} for todo in todos]
+    todos = todos_collection.find({}, {"_id": 1, "text": 1, "order": 1}).sort("order", 1)
+    return [{"_id": str(todo["_id"]), "text": todo["text"], "order": todo.get("order", 0)} for todo in todos]
 
 
 def add_todo(text):
-    todo = {"text": text}
+    max_order_todo = todos_collection.find_one(sort=[("order", -1)])
+    next_order = (max_order_todo["order"] + 1) if max_order_todo and "order" in max_order_todo else 0
+    todo = {"text": text, "order": next_order}
     todos_collection.insert_one(todo)
 
 
@@ -218,3 +220,11 @@ def update_todo(todo_id, text):
 
 def delete_todo(todo_id):
     todos_collection.delete_one({"_id": ObjectId(todo_id)})
+
+
+def reorder_todos(ids_in_order):
+    for index, todo_id in enumerate(ids_in_order):
+        todos_collection.update_one(
+            {"_id": ObjectId(todo_id)},
+            {"$set": {"order": index}}
+        )
